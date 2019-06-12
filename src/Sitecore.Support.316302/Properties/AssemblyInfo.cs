a9a3c433e273a87b634acbdab33f2ca03db7d7e0
Vladimir Taran <vlt@sitecore.net>
2019-06-12 13:29:08 -0400

    Fixes excessive ClientData rewrites caused by LoginVirtualUser method when Federated Authentication is enabled.

---
+++ src/Sitecore.Support.316302/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.316302")]
+[assembly: AssemblyProduct("Sitecore.Support.316302")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.316302/SqlServerClientDataStore.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Common;
+using Sitecore.Data.DataProviders.Sql;
+using Sitecore.Eventing;
+using Sitecore.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Data.SqlServer
+{
+    public class SqlServerClientDataStore : Sitecore.Data.SqlServer.SqlServerClientDataStore
+    {
+        public SqlServerClientDataStore(SqlDataApi api, string objectLifetime, IEventQueue queue, BaseEventManager eventManager) : base(api, objectLifetime, queue, eventManager)
+        {
+
+        }
+
+        public SqlServerClientDataStore(string connectionString, string objectLifetime, IEventQueue queue, BaseEventManager eventManager) : base(connectionString, objectLifetime, queue, eventManager)
+        {
+           
+        }
+
+        protected override string GetDataKey()
+        {
+            if (!string.IsNullOrEmpty(Switcher<string, string>.CurrentValue))
+            {
+                string key = Switcher<string, string>.CurrentValue;
+                if (key != Sitecore.Owin.Authentication.Constants.LocalIdentityProvider)
+                {
+                    return key;
+                }
+            }
+            return WebUtil.GetSessionID();
+        }
+    }
+}
